72b3c474562d7c51df5d158ef3356d76685935c3
Christian Ashley <christianzashley@gmail.com>
2023-12-08 13:35:35 -0500

    Solve part 2 of day 1
    * Add string to accumulate characters and check again digit dictionary

--- day1/trebuchet.ts
+++ day1/trebuchet.ts
@@ -12,12 +12,50 @@ function sumCalibration(sum: number, cal: string): number {
   return sum + parseInt(filterFirstAndLastDigits(cal))
 }
 
+const digitDict = Object.freeze({
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9'
+})
+const digitList = Object.freeze(Object.keys(digitDict) as Array<keyof typeof digitDict>)
+
 function filterFirstAndLastDigits(str: string): string {
   let first = '', last = ''
+  let frontAcc = '', backAcc = ''
 
   for (let i = 0; !(first && last) && i < str.length; i++) {
-    if (!first && str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57) first = str[i]
-    if (!last && str.charCodeAt(str.length - 1 - i) >= 48 && str.charCodeAt(str.length - 1 - i) <= 57) last = str[str.length - 1 - i]
+    if (!first) {
+      if (str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57) {
+        first = str[i]
+      } else {
+        frontAcc += str.charAt(i)
+        for (let j = 0; j < digitList.length; j++) {
+          if (frontAcc.endsWith(digitList[j])) {
+            first = digitDict[digitList[j]]
+            break
+          }
+        }
+      }
+    }
+    if (!last) {
+      if (str.charCodeAt(str.length - 1 - i) >= 48 && str.charCodeAt(str.length - 1 - i) <= 57) {
+        last = str[str.length - 1 - i]
+      } else {
+        backAcc = str.charAt(str.length - 1 - i) + backAcc
+        for (let j = 0; j < digitList.length; j++) {
+          if (backAcc.startsWith(digitList[j])) {
+            last = digitDict[digitList[j]]
+            break
+          }
+        }
+      }
+    }
   }
 
   return first + last
