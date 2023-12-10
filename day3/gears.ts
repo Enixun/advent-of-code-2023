4d7035d536e0b6c0f56acdc6d157dc27b8af6ec7
Christian Ashley <christianzashley@gmail.com>
2023-12-10 16:23:17 -0500

    Solve part 2 of day 3
    * Make matrix traversal decoupled from function dfNumSearch
    * Add function reduceMatrix and change dfsMatrix to traverseMatrix
    * TODO: Make type Matrix more generic, introduce matrix class and adapt methods

--- day3/gears.ts
+++ day3/gears.ts
@@ -6,10 +6,13 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
-function generateMatrix(filename: string): string[][] {
+// type Matrix<T> = T[][] //Array<Array<T>>
+type Matrix = string[][]
+
+function generateMatrix(filename: string) {
   const contents = fs.readFileSync(path.resolve(__dirname, filename), 'utf-8')
 
-  const matrix: string[][] = [];
+  const matrix: Matrix = [];
   let row: string[] = [];
   for (let i = 0; i < contents.length; i++) {
     if (contents[i] === '\n') {
@@ -24,55 +27,89 @@ function generateMatrix(filename: string): string[][] {
 }
 
 interface PartNumber {
-  char: string,
+  value: string,
   isValid: boolean
 }
 
-function dfsMatrix(m: string[][]): number {
-  const searchDirections = [
-    [1, 0], //immediate right first, to capture whole number
-    [0, -1], // top
-    [0, 1], // bottom
-    [-1, 0], //left
-    [-1, -1], // top left
-    [1, -1], // top right
-    [-1, 1], // bottom left
-    [1, 1], // bottom right
-  ];
+type Coordinates = [x: number, y: number]
 
-  let partSum = 0
+type dfsHelper = (y: number, x: number, m: Matrix) => PartNumber | number
 
-  function dfsHelper(y: number, x: number): PartNumber {
-    if (Number.isNaN(parseInt(m[y][x]))) return { char: '', isValid: m[y][x] !== '.' }
-    
-    const partNum: PartNumber = {
-      char: m[y][x],
-      isValid: false,
+const searchDirections: Coordinates[] = [
+  [1, 0], //immediate right first, to capture whole number
+  [-1, 0], //left
+  [0, -1], // top
+  [0, 1], // bottom
+  [-1, -1], // top left
+  [1, -1], // top right
+  [-1, 1], // bottom left
+  [1, 1], // bottom right
+];
+
+function dfNumSearch(y: number, x: number, m: Matrix): PartNumber {
+  if (Number.isNaN(parseInt(m[y][x]))) return { value: '', isValid: m[y][x] !== '.' }
+  
+  const partNum: PartNumber = {
+    value: m[y][x],
+    isValid: false,
+  }
+  m[y][x] = '.'
+  
+  for (let i = 0; i < searchDirections.length; i++) {
+    const yOffset = y + searchDirections[i][1], xOffset = x + searchDirections[i][0]
+
+    if (yOffset >= 0 && yOffset < m.length &&
+        xOffset >= 0 && xOffset < m[y].length) {
+          const adjacent = dfNumSearch(yOffset, xOffset, m)
+          if (i === 0) partNum.value += adjacent.value
+          if (i === 1) partNum.value = adjacent.value + partNum.value
+          partNum.isValid = adjacent.isValid || partNum.isValid
     }
-    m[y][x] = '.'
-    
-    for (let i = 0; i < searchDirections.length; i++) {
-      if (y + searchDirections[i][1] >= 0 && 
-          y + searchDirections[i][1] < m.length &&
-          x + searchDirections[i][0] >= 0 &&
-          x + searchDirections[i][0] < m[y].length) {
-            const adjacent = dfsHelper(y + searchDirections[i][1], x + searchDirections[i][0])
-            if (i === 0) partNum.char += adjacent.char
-            partNum.isValid = adjacent.isValid || partNum.isValid
-      }
   }
 
-    return partNum
+  return partNum
+}
+
+function dfGearSearch(y: number, x: number, m: Matrix): number {
+  if (m[y][x] !== '*') return 0
+  // console.log(m.map(e => e.join('')).join('\n'))
+
+  const gearNums: number[] = []
+
+  for (let i = 0; i < searchDirections.length; i++) {
+    const yOffset = y + searchDirections[i][1], xOffset = x + searchDirections[i][0]
+
+    if (yOffset >= 0 && yOffset < m.length &&
+        xOffset >= 0 && xOffset < m[y].length) {
+          const partNum = dfNumSearch(yOffset, xOffset, m)
+          if (partNum.isValid) gearNums.push(parseInt(partNum.value))
+    }
   }
 
+  m[y][x] = '.'
+
+  return gearNums.length === 2 ? gearNums[0] * gearNums[1] : 0
+}
+
+function traverseMatrix(m: Matrix, cb: (col: number, row: number) => void): void {
   for (let i = 0; i < m.length; i++) {
     for (let j = 0; j < m[i].length; j++) {
-      const partNum = dfsHelper(i, j)
-      if (partNum.isValid && partNum.char.length > 0) partSum += parseInt(partNum.char)
+      cb(i, j)
     }
   }
+}
+
+function reduceMatrix(m: Matrix, fn: dfsHelper): number {
+  let partSum = 0
+
+  traverseMatrix(m, (i, j) => {
+    const partNum = fn(i, j, m)
+    if (typeof partNum === 'number') partSum += partNum
+    else if (partNum.isValid && partNum.value.length > 0) partSum += parseInt(partNum.value)
+  })
 
   return partSum
 }
 
-console.log(dfsMatrix(generateMatrix('schematic.txt')))
+console.log('sum of numbers touching symbols:', reduceMatrix(generateMatrix('schematic.txt'), dfNumSearch))
+console.log('sum of powers touching *:', reduceMatrix(generateMatrix('schematic.txt'), dfGearSearch))
