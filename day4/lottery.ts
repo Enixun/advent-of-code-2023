fc62c0ba2ddae2f84da88d2d513bb4c68645abd4
Christian Ashley <christianzashley@gmail.com>
2023-12-11 12:36:24 -0500

    Solve part 2 of day 4
    * Write Game class to hold id, winning numbers, play numbers, matches, and count
    * Convert file string to array splitting at new lines, map Games, and reduce

--- day4/lottery.ts
+++ day4/lottery.ts
@@ -3,6 +3,30 @@ import path from 'node:path'
 
 const cardsStr = fs.readFileSync(path.resolve(__dirname, 'cards.txt'), 'utf-8')
 
+class Game {
+  id: string | null = null
+  winNums: Set<string> | null = null
+  playNums: Set<string> | null = null
+  matches: Set<string> | null = null
+  count = 1
+
+  constructor(str: string) {
+    const theBits = /^(?:Card +)(?<id>\d+)(?:: +)(?<winNums>(\d+\s*?)*)(?: \| +)(?<playNums>[0-9 ]*)$/.exec(str)
+    // console.log(theBits)
+    
+    if (theBits?.groups) {
+      this.id = theBits.groups.id
+      this.winNums = new Set(theBits.groups.winNums.split(/\s+/))
+      this.playNums = new Set()
+      this.matches = new Set()
+      theBits.groups.playNums.split(/\s+/).forEach(num => {
+        this.playNums?.add(num)
+        if (this.winNums?.has(num)) this.matches?.add(num)
+      })
+    }
+  }
+}
+
 function getWinnings(fileContents: string): number {
   let amount = 0
   for (let i = 0; i < fileContents.length; i++) {
@@ -55,4 +79,15 @@ function getWinnings(fileContents: string): number {
   return amount
 }
 
-console.log(getWinnings(cardsStr))
+const cardsArr = cardsStr.trim().split(/\n/)
+
+console.log('part 1 winnings', getWinnings(cardsStr))
+console.log('part 2 number of cards', cardsArr.map(gameStr => new Game(gameStr)).reduce((acc, cur, ind, arr) => {
+  for (let c = 1; c <= cur.count; c++) {
+    for (let i = 1; i <= Math.min(arr.length - 1 - ind, cur.matches?.size ?? 0); i++) {
+      arr[ind + i].count++
+    }
+  }
+
+  return acc += cur.count
+}, 0))
