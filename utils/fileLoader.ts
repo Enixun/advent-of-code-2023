5d0303cc53f19192535072c7209a8ca3bc7ab48a
Christian Ashley <christianzashley@gmail.com>
2023-12-13 13:56:38 -0500

    Update utiles/fileLoader
    * Make separator an optional paramater, split at its value if present

--- utils/fileLoader.ts
+++ utils/fileLoader.ts
@@ -3,11 +3,7 @@ import path from 'node:path'
 
 const calledDir = process.argv[1].slice(0, process.argv[1].lastIndexOf('/'))
 
-// export function parseFile(filePath: string): string {
-//   const fileContents = fs.readFileSync(path.resolve(calledDir, filePath), 'utf-8')
-//   return fileContents
-// }
-export function parseFile(filePath: string, separator: string | RegExp): string[] {
-  const fileContents = fs.readFileSync(path.resolve(calledDir, filePath), 'utf-8')
-  return fileContents.trim().split(separator)
+export function parseFile(filePath: string, separator?: string | RegExp): string | string[] {
+  const fileContents = fs.readFileSync(path.resolve(calledDir, filePath), 'utf-8').trim()
+  return separator ? fileContents.split(separator) : fileContents
 }
