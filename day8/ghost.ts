78c54bafb3f89510b235a7975f2e323d994f13e6
Christian Ashley <christianzashley@gmail.com>
2023-12-15 00:16:27 -0500

    Update day8/ghost.ts
    * Add SearchNodeCellection class to track starting nodes
    * Refactor existing code to work with this format

--- day8/ghost.ts
+++ day8/ghost.ts
@@ -5,24 +5,36 @@ type SearchNode = {
   R: string
 }
 
-const [ directions, nodesArr ] = parseFile('nodes.txt', /(?:\r?\n){2}/) as string[]
+class SearchNodeCollection {
+  nodes: { [k: string]: SearchNode } = {}
+  startNodes: SearchNode[] = []
+  directions: string
+  constructor(file: string) {
+    const [ directions, nodesArr ] = parseFile(file, /(?:\r?\n){2}/) as string[]
 
-const nodes: { [k: string]: SearchNode } = {}
+    nodesArr.split(/\r?\n/).forEach(n => {
+      const node = n.match(/^(?<name>\w{3}) = \((?<left>\w{3}), (?<right>\w{3})\)$/)
+      if (node === null || !node.groups) throw new Error(`Pattern not met: invalid node ` + n)
+      const { name, left, right } = node.groups
+      this.nodes[name] = { L: left, R: right }
+    })
 
-nodesArr.split(/\r?\n/).forEach(n => {
-  const node = n.match(/^(?<name>\w{3}) = \((?<left>\w{3}), (?<right>\w{3})\)$/)
-  if (node === null || !node.groups) throw new Error(`Pattern not met: invalid node ` + n)
-  const { name, left, right } = node.groups
-  nodes[ name ] = { L: left, R: right }
-})
+    if (directions.match(/[^LR]/) !== null) throw new Error('Invalid search directions: Must be sequence of Ls and/or Rs')
+    this.directions = directions
+  }
 
-let curNode = 'AAA', i = 0, counter = 0
-if (directions.match(/[^LR]/) !== null) throw new Error('Invalid search directions: Must be sequence of Ls and/or Rs')
+  traverse() {
+    let curNode = 'AAA', i = 0, counter = 0
+    
+    while (curNode !== 'ZZZ' && i < this.directions.length) {
+      curNode = this.nodes[curNode][this.directions[i] as 'L' | 'R']
+      if (++i >= this.directions.length) i = 0
+      counter += 1
+    }
 
-while (curNode !== 'ZZZ' && i < directions.length) {
-  curNode = nodes[curNode][directions[i] as 'L' | 'R']
-  if (++i >= directions.length) i = 0
-  counter += 1
+    return counter
+  }
 }
 
-console.log('Number of steps:', counter)
+const test = new SearchNodeCollection('nodes.txt')
+console.log('Number of steps:', test.traverse())
