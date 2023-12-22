8abc8f8cb237fad5a82ea4e92026eb869d767263
Christian Ashley <christianzashley@gmail.com>
2023-12-22 14:46:31 -0500

    Solve part 2 of day 11
    * Modify GalaxyMatrix to add emptyScale in constructor, used to calculate value of space
    * Instead of drawing matrix to scale, replace empty rows/columns with '*' and calculate
    * Use the calculated position of galaxies for Graph nodes

--- day11/cosmos.ts
+++ day11/cosmos.ts
@@ -4,7 +4,7 @@ class Graph {
   static idCount = 1
   id: number
   position: Coordinates
-  children: { distance: number, node: Graph}[]
+  children: { distance: number, node: Graph }[]
   constructor(position: Coordinates) {
     this.id = Graph.idCount++
     this.position = position
@@ -14,7 +14,7 @@ class Graph {
 
 class GalaxyMatrix extends Matrix {
   graph: Graph[] = []
-  constructor(file: string) {
+  constructor(file: string, emptyScale: number) {
     super(file)
 
     for (let j = 0; j < this.matrix[0].length; j++) {
@@ -26,21 +26,22 @@ class GalaxyMatrix extends Matrix {
         }
       }
       if (emptyCol) {
-        this.matrix = this.matrix.map(row => [...row.slice(0, j), '.', ...row.slice(j)])
-        j += 1
+        this.matrix = this.matrix.map(row => [...row.slice(0, j), '*', ...row.slice(j + 1)])
       }
     }
-    const rowMap: typeof this.matrix = []
-    this.matrix = this.matrix.reduce((acc, cur) => {
-      acc.push(cur)
-      if (cur.every(el => el === '.')) acc.push(cur)
-      return acc
-    }, rowMap)
+
+    this.matrix = this.matrix.map((row) => {
+      return (row.every(el => el !== '#')) ? row.map(_ => '*') : row
+    })
 
     this.traverse((val, pos, m) => {
       if (val === '#') {
-        const g = new Graph(pos)
-        this.graph = this.graph.map(n => {
+        const calcPos: Coordinates = [
+          m.matrix.slice(0, pos[0]).reduce((acc, cur) => acc + (cur[pos[1]] !== '*' ? 1 : emptyScale), 0),
+          m.matrix[pos[0]].slice(0, pos[1]).reduce((acc, cur) => acc + (cur !== '*' ? 1 : emptyScale), 0),
+        ]
+        const g = new Graph(calcPos)
+        m.graph = m.graph.map(n => {
           const distance = Math.abs(n.position[0] - g.position[0]) + Math.abs(n.position[1] - g.position[1])
           n.children.push({
             distance,
@@ -48,13 +49,14 @@ class GalaxyMatrix extends Matrix {
           })
           return n
         })
-        this.graph.push(g)
+        m.graph.push(g)
       }
     })
   }
 }
 
-const g = new GalaxyMatrix('galaxies.txt')
+const g = new GalaxyMatrix('galaxies.txt', 1000000)
+// g.print()
 console.log(g.graph.reduce((acc: number, cur) => {
   return acc + cur.children.reduce((acc: number, cur) => acc + cur.distance, 0)
 }, 0))
